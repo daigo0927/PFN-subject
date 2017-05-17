38e7c260a2f1335a2a51acd5b10dd3e81a5ab9af
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-05-17 13:04:02 +0900

    add subject1 code

---
+++ subject1.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+
+import math
+import random
+import sys, os
+
+class EasyEnv:
+
+    def __init__(self):
+
+        self.cleared = True
+        self.reward = None
+        self.prev_obs = None
+        self.obs = None
+        self.count = None
+        self.terminate = False
+
+    def reset(self):
+
+        # sampling initial obserbation from Uniform[-1, 1]
+        self.prev_obs = random.uniform(-1, 1)
+
+        self.count = 0
+
+        return [self.prev_obs]
+
+    def obs_dim(self):
+
+        return 1
+
+    def step(self, action):
+
+        # sampling current obserbation
+        self.obs = random.uniform(-1, 1)
+
+        self.reward = action * self.prev_obs
+
+        self.prev_obs = self.obs
+
+        self.count += 1
+        if self.count == 10:
+            self.terminate = True
+
+        return [self.obs], self.terminate, self.reward
+
+
+def test():
+    env = EasyEnv()
+    print('env.reset() : {}'.format(env.reset()))
+    print('env.obs_dim() : {}'.format(env.obs_dim()))
+    for i in range(10):
+        act = random.choice([-1, 1])
+        print('step{} env.step(action={}) : {}'\
+              .format(env.count, act, env.step(action = act)))
+
+
+if __name__ == '__main__':
+    test()
